2778687700e8d6d49e73dae605173887e3ad8bd9
Nikolay Gorbachev <nickgor0303@gmail.com>
2020-11-01 17:18:59 +0000

    Update VPTree.h

--- prj.lab/VPTree/VPTree.h
+++ prj.lab/VPTree/VPTree.h
@@ -1,14 +1,10 @@
+#include <stdlib.h>
 #include <algorithm>
-#include <array>
-#include <cmath>
-#include <iostream>
-#include <random>
 #include <vector>
-#include <tgmath.h>
+#include <stdio.h>
 #include <queue>
-
-#ifndef VPTREE1_VPTREE_H
-#define VPTREE1_VPTREE_H
+#include <limits>
+#include <array>
 
 
 class Point {
@@ -32,248 +28,131 @@ public:
         }
         return dist;
     }
+    bool operator==(const Point& pt) const{
+        return (getCoordByIdx(0) == pt.getCoordByIdx(0)) && (getCoordByIdx(1) == pt.getCoordByIdx(1)) && (getCoordByIdx(2) == pt.getCoordByIdx(2));
+    }
+    bool inRange(const Point& p, double radius) const{
+        return getDistance(p) < radius;
+    }
+    void printPoint() const{
+        for (size_t i = 0; i < 3; ++i) {
+            if (i > 0)
+                std::cout << ", ";
+            std::cout << getCoordByIdx(i);
+        }
+    }
 private:
     std::array<float, 3> coordinates;
 };
 
 
-
-
-
-class VPTree {
-
-private:
-
-
-    struct Node {
-
-
-        Node(const Point& inputPoint) : pivotPoint(inputPoint),
-                                        leftChildNode(nullptr), rightChildNode(nullptr){}
-
-        float getPointCoordByIdx(size_t index) const {
-            return pivotPoint.getCoordByIdx(index);
+class VPTree{
+public:
+    VPTree(std::vector<Point>& points)
+            : pointsNumber(points.size()) {
+        if (pointsNumber == 0) {
+            // empty point set, return empty tree
+            root      = nullptr;
+            leftChildNode   = nullptr;
+            rightChildNode  = nullptr;
+            leaf = true;
+            borderDistance = std::numeric_limits<double>::infinity();
         }
-
-        double distance(const Point& pt) const {
-            return pivotPoint.getDistance(pt);
+        else if (pointsNumber == 1) {
+            root = new Point(points[0]);
+            leftChildNode = nullptr;
+            rightChildNode = nullptr;
+            leaf = true;
+            borderDistance = std::numeric_limits<double>::infinity();
         }
-
-        Point pivotPoint;
-        double threshold;
-        Node* leftChildNode;
-        Node* rightChildNode;
-
-    };
-
-
-
-    struct DistanceComparator
-    {
-        const Point item;
-        DistanceComparator(const Point item ) : item(item) {}
-        bool operator()(const Point a, const Point b) {
-            return item.getDistance(a) < item.getDistance(b);
+        else {
+            root = new Point(points[0]);
+            leaf = false;
+            decltype(points.size()) firstIdx = 1;
+            decltype(points.size()) lastIdx  = points.size() - 1;
+            decltype(points.size()) middIdx  = (firstIdx + lastIdx) / 2;
+            auto firstIter = points.begin() + firstIdx;
+            auto middIter  = points.begin() + middIdx;
+            std::nth_element( firstIter, middIter, points.end(), [&](const Point & p1, const Point & p2)->bool {
+                return root->getDistance(p1) < root->getDistance(p2);
+            });
+            borderDistance = root->getDistance(points[middIdx]);
+            decltype(middIdx) leftLen   = middIdx - firstIdx + 1;
+            decltype(middIdx) rightLen  = lastIdx - middIdx;
+            std::vector<Point> left, right;
+            std::copy_n(firstIter, leftLen,  std::back_inserter(left));
+            std::copy_n(middIter + 1, rightLen, std::back_inserter(right));
+            leftChildNode  = left.empty() ? nullptr : new VPTree(left);
+            rightChildNode = right.empty() ? nullptr : new VPTree(right);
         }
-    };
-
-
-
-
-
-
-
-
-
-
-
-    Node* buildTree(size_t begin, size_t end, size_t index){
-        if (end <= begin)
-            return nullptr;
-        size_t middle = begin + (end - begin) / 2;
-        std::nth_element(&nodeVector[begin], &nodeVector[middle], &nodeVector[0] + end, DistanceComparator(nodeVector[index].pivotPoint));
-        index = (index + 1) % 3;
-        nodeVector[middle].leftChildNode = buildTree(begin, middle, index);
-        nodeVector[middle].rightChildNode = buildTree(middle + 1, end, index);
-        return &nodeVector[middle];
     }
-
-
-    Node* buildFromPoints( int lower, int upper )
-    {
-        if ( upper == lower ) {
-            return NULL;
-        }
-
-        Node node(nodeVector[lower]);
-
-        if ( upper - lower > 1 ) {
-
-            int i = (int)((double)rand() / RAND_MAX * (upper - lower - 1) ) + lower;
-            std::swap( nodeVector[lower], nodeVector[i] );
-            int median = ( upper + lower ) / 2;
-
-
-            std::nth_element(
-                    nodeVector.begin() + lower + 1,
-                    nodeVector.begin() + median,
-                    nodeVector.begin() + upper,
-                    DistanceComparator( nodeVector[lower].pivotPoint ));
-
-            node.threshold = nodeVector[lower].pivotPoint.getDistance(nodeVector[median].pivotPoint);
-            node.pivotPoint = nodeVector[lower].pivotPoint;
-            node.leftChildNode = buildFromPoints( lower + 1, median );
-            node.rightChildNode = buildFromPoints( median, upper );
-        }
-
-        return &node;
+    ~VPTree() {
+        delete root;
+        delete leftChildNode;
+        delete rightChildNode;
     }
-
-
-
-
-    Node* kNearestBuildTree(size_t begin, size_t end, size_t step, size_t k){
-        if (end <= begin)
-            return nullptr;
-        size_t middle = begin + (end - begin) / 2;
-        std::nth_element(&nodeVector[begin], &nodeVector[middle], &nodeVector[0] + end, DistanceComparator(nodeVector[middle].pivotPoint));
-        ++step;
-        size_t index = step % 3;
-        if (step < log2 (nodeVector.size() / k)){
-            nodeVector[middle].leftChildNode = kNearestBuildTree(begin, middle, step, k);
-            nodeVector[middle].rightChildNode = kNearestBuildTree(middle + 1, end, step, k);
-
-        }
-        return &nodeVector[middle];
+    bool isLeaf() const {
+        return leaf;
     }
-
-
-
-
-
-    void closestPointFromIdxStep(Node* tRoot, const Point& point, int index){
-        if (tRoot == nullptr)
-            return;
-        ++visited;
-        double d = tRoot->distance(point);
-        if (d < bestDist || best == nullptr) {
-            bestDist = d;
-            best = tRoot;
+    void runThroughTree() const {
+        if (root) {
+            root->printPoint();
+            if (leftChildNode)
+                leftChildNode->runThroughTree();
+            if (rightChildNode)
+                rightChildNode->runThroughTree();
         }
-
-        double dx = tRoot->getPointCoordByIdx(index) - point.getCoordByIdx(index);
-        index = (index + 1) % 3;
-        closestPointFromIdxStep(dx > 0 ? tRoot->leftChildNode : tRoot->rightChildNode, point, index);
-
-        if (bestDist <= dx * dx) // this here added
-            return;
-        closestPointFromIdxStep(dx > 0 ? tRoot->rightChildNode : tRoot->leftChildNode, point, index);
-    };
-
-
-    void kClosestPointsFromIdxStep(Node* tRoot, const Point& point, int k, int step, size_t begin, size_t end){
-        if (tRoot == nullptr)
-            return;
-        ++visited;
-
-        double d = tRoot->distance(point);
-        if (d < bestDist || best == nullptr) {
-            bestDist = d;
-            best = tRoot;
+        else
+            std::cout << "empty tree" << std::endl;
+    }
+    const VPTree* closestNode(Point& pt) const {
+        VPTree* node = nullptr;
+        if (pt == (*root))
+            return this;
+        else if (leaf)
+            node = nullptr;
+        else {
+            double distance = root->getDistance(pt);
+            if (distance > borderDistance)
+                return rightChildNode->closestNode(pt);
+            else
+                return leftChildNode->closestNode(pt);
         }
-
-        double dx = tRoot->getPointCoordByIdx(step % 3) - point.getCoordByIdx(step % 3);
-
-
-        ++step;
-        size_t index = step % 3;
-
-
-        if (step < log2 (nodeVector.size() / k)){
-            kClosestPointsFromIdxStep(dx > 0 ? tRoot->leftChildNode : tRoot->rightChildNode, point, k, step, begin, begin + (end - begin) / 2);
-            if (bestDist <= dx * dx) // this here added
-                return;
-            kClosestPointsFromIdxStep(dx > 0 ? tRoot->rightChildNode : tRoot->leftChildNode, point, k, step, begin + (end - begin) / 2 + 1, end);
-
+        return node;
+    }
+    void search(const Point& pt, double radius, std::vector<Point>& foundPoints) const {
+        double distance = root->getDistance(pt);
+        if (0 < distance && distance <= radius)
+            foundPoints.push_back(*root);
+        if (leaf) {
+            return;
         }
-
-
-
-        if ((step >= log2(nodeVector.size() / k)) && (step < log2(nodeVector.size()))){
-
-            int i;
-            for (i = 0; i < end - begin; ++i){
-
-                d = point.getDistance(nodeVector[i + begin].pivotPoint);
-                if ((d < bestDist) || (bestDist == 0)) {
-                    bestDist = d;
-                    best = &nodeVector[i + begin];
-                }
-
+        else if (distance > borderDistance) {
+            if (rightChildNode)
+                rightChildNode->search(pt, radius, foundPoints);
+            if (distance - borderDistance < radius && leftChildNode) {
+                leftChildNode->search(pt, radius, foundPoints);
+            }
+            else {
             }
         }
-
-
-        return;
-
-    };
-
-
-
-
-    std::vector<Node> nodeVector;
-    Node* root = nullptr;
-    Node* best = nullptr;
-    double bestDist = 0;
-    size_t visited = 0;
-    double _tau;
-
-
-public:
-
-    VPTree(const VPTree&) = delete;
-    VPTree& operator=(const VPTree&) = delete;
-
-
-    template<typename iterator>
-    VPTree(iterator begin, iterator end) : nodeVector(begin, end) {
-        root = buildTree(0, nodeVector.size(), 0);
+        else
+        {
+            if (leftChildNode)
+                leftChildNode->search(pt, radius, foundPoints);
+            if (distance + radius > borderDistance && rightChildNode) {
+                rightChildNode->search(pt, radius, foundPoints);
+            }
+            else {
+            }
+        }
     }
-
-    template<typename iterator>
-    VPTree(iterator begin, iterator end, int k) : nodeVector(begin, end) {
-        root = kNearestBuildTree(0, nodeVector.size(), 0, k);
-    }
-
-
-    size_t totalVisited() const { return visited; }
-
-    double distance() const { return std::sqrt(bestDist); }
-
-
-    const Point& closestPoint(const Point& pt) {
-        if (root == nullptr)
-            throw std::logic_error("tree is empty");
-        best = nullptr;
-        visited = 0;
-        bestDist = 0;
-        closestPointFromIdxStep(root, pt, 0);
-        return best->pivotPoint;
-    }
-
-    const Point& kClosestPoints(const Point& pt, int k) {
-        if (root == nullptr)
-            throw std::logic_error("tree is empty");
-        best = nullptr;
-        visited = 0;
-        bestDist = 0;
-        kClosestPointsFromIdxStep(root, pt, k, 0, 0, nodeVector.size());
-        return best->pivotPoint;
-    }
-
-
+private:
+    Point* root;
+    double borderDistance;
+    bool leaf;
+    size_t pointsNumber;
+    VPTree* leftChildNode;
+    VPTree* rightChildNode;
 };
 
-
-
-#endif //VPTREE1_VPTREE_H
